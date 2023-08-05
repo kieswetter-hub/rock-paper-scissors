c2e9a9237a54caeb9df44a00800eb0ba32acadcc
kieswetter-hub <50299190+kieswetter-hub@users.noreply.github.com>
2023-08-05 11:23:17 +0200

    add pseudocode to java

---
+++ rock-paper-scissors.js
@@ -0,0 +1,30 @@
+/* rock paper scissors */
+
+/* pseudocode 
+create list with text values: rock, paper, or scissors
+randomly choose one for computer choice and store as variable computerChoice
+prompty user for choice and store as variable userChoice
+
+define values:
+rock greater than scissors
+paper greater than rock
+scissors greater than paper
+
+create variable computerScore
+create variable userScore
+
+if computerChoice is greater than userChoice 
+    return message you lose
+    add one to computerScore
+if userChoice is greater than computerChoice 
+    return message you win
+    add one to userScore
+
+loop game for five rounds
+
+after five rounds 
+if computerScore is greater than userScore
+    return you lost
+if userScore is greater than computerScore
+    return you win
+*/
